9dd9cdbee1495849ad51fbc671827a55ab8d1936
Zver <SvetlanaEvcheko@yandex.ru>
2024-09-27 19:36:33 +0300

    Initial

---
+++ src/test/java/com/homework/Spring_Mockito/SpringMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.Spring_Mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
